84bbd9d41158d4d4580f59c29bf46ad428d87e84
chnbits <riskcn@163.com>
2021-04-27 11:54:01 +0800

    控制器调整

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Oper_record;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BaseController extends Controller
 {
+
+    protected $admin_table = 'admins';
+    protected $role_table = 'roles';
+    protected $menu_table = 'menus';
+    protected $dict_table = 'dict';
+    protected $dictData_table = 'dict_data';
 
     public function __construct(Request $request)
     {
@@ -17,11 +24,7 @@ class BaseController extends Controller
         $this->parm = json_encode($request->all());
     }
 
-    public function res($code,$msg,$count='',$data='')
-    {
-        return response()->json(['code'=>$code,'msg'=>$msg,'count'=>$count,'data'=>$data]);
-    }
-
+    //操作记录
     public function opRec($userId,$model,$description,$state='0')
     {
         $parms = array(
@@ -36,5 +39,37 @@ class BaseController extends Controller
             'createTime'=>date('Y-m-d H:i:s',time())
         );
         Oper_record::index($parms);
+    }
+
+    //插入数据
+    public function insertData($table,$data)
+    {
+        $res = DB::table($table)->insert($data);
+        return $res;
+    }
+    //更新数据
+    public function updateData($table,$column,$id,$data)
+    {
+        $data['updateTime'] = date('Y-m-d H:i:s', time());
+        $res = DB::table($table)->where($column, $id)->update($data);
+        return $res;
+    }
+    //删除单个数据
+    public function deleteData($table,$column,$id)
+    {
+        $res = DB::table($table)->where($column, $id)->update(['deleted' => 1, 'updateTime' => date('Y-m-d H:i:s', time())]);
+        return $res;
+    }
+    //删除多行数据
+    public function deletePatch($table,$column,$ids)
+    {
+        $res = DB::table($table)->whereIn($column,$ids)->update(['deleted'=>1,'updateTime' => date('Y-m-d H:i:s', time())]);
+        return $res;
+    }
+
+    //返回结果
+    public function res($code,$msg,$count='',$data='')
+    {
+        return response()->json(['code'=>$code,'msg'=>$msg,'count'=>$count,'data'=>$data]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\Admin\FileController;
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\System\DictController;
+use App\Http\Controllers\Admin\System\LogController;
+use App\Http\Controllers\Admin\System\MenuController;
+use App\Http\Controllers\Admin\System\RoleController;
+use App\Http\Controllers\Admin\System\UserController;
 use App\Http\Controllers\Admin\SystemController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,37 +31,37 @@ Route::group(['middleware'=>'auth_passport'],function (){
     Route::get('/main/user',[MainController::class,'getAdmin']);
     Route::get('/main/menu',[MainController::class,'menus']);
 
-    Route::get('/sys/loginRecord/page',[SystemController::class,'loginRecord']);
-    Route::get('/sys/operRecord/page',[SystemController::class,'oplog']);
+    Route::get('/sys/loginRecord/page',[LogController::class,'loginRecord']);
+    Route::get('/sys/operRecord/page',[LogController::class,'oplog']);
 
-    Route::get('/sys/user/page',[SystemController::class,'getUserPage']);
-    Route::get('/sys/role',[SystemController::class,'getRole']);
-    Route::put('/sys/user/state/{id}',[SystemController::class,'changeUserState']);
-    Route::put('/sys/user',[SystemController::class,'editUser']);
-    Route::post('/sys/user',[SystemController::class,'createUser']);
-    Route::post('sys/user/import',[SystemController::class,'importUser']);
-    Route::post('/sys/user/{id}',[SystemController::class,'deleteUser']);
-    Route::post('sys/user/batch',[SystemController::class,'deleteUser']);
+    Route::get('/sys/user/page',[UserController::class,'getUserPage']);
+    Route::get('/sys/role',[UserController::class,'getRole']);
+    Route::put('/sys/user/state/{id}',[UserController::class,'changeUserState']);
+    Route::put('/sys/user',[UserController::class,'editUser']);
+    Route::post('/sys/user',[UserController::class,'createUser']);
+    Route::post('sys/user/import',[UserController::class,'importUser']);
+    Route::post('/sys/user/{id}',[UserController::class,'deleteUser']);
+    Route::post('sys/user/batch',[UserController::class,'deleteUser']);
 
 
-    Route::get('/sys/role/page',[SystemController::class,'getRolePage']);
-    Route::get('/sys/role/menu',[SystemController::class,'getRoleMenu']);
-    Route::put('/sys/role',[SystemController::class,'editRole']);
-    Route::post('/sys/role',[SystemController::class,'createRole']);
-    Route::put('/sys/role/{id}',[SystemController::class,'deleteRole']);
-    Route::put('/sys/role/batch',[SystemController::class,'deleteRole']);
-    Route::put('/sys/role/menu/{id}',[SystemController::class,'editRoleRight']);
+    Route::get('/sys/role/page',[RoleController::class,'getRolePage']);
+    Route::get('/sys/role/menu',[RoleController::class,'getRoleMenu']);
+    Route::put('/sys/role',[RoleController::class,'editRole']);
+    Route::post('/sys/role',[RoleController::class,'createRole']);
+    Route::put('/sys/role/{id}',[RoleController::class,'deleteRole']);
+    Route::put('/sys/role/batch',[RoleController::class,'deleteRole']);
+    Route::put('/sys/role/menu/{id}',[RoleController::class,'editRoleRight']);
 
-    Route::get('/sys/menu',[SystemController::class,'getMenu']);
-    Route::put('/sys/menu',[SystemController::class,'editMenu']);
-    Route::put('/sys/menu/{id}',[SystemController::class,'deleteMenu']);
+    Route::get('/sys/menu',[MenuController::class,'getMenu']);
+    Route::put('/sys/menu',[MenuController::class,'editMenu']);
+    Route::put('/sys/menu/{id}',[MenuController::class,'deleteMenu']);
 
-    Route::get('/sys/dict',[SystemController::class,'getDict']);
-    Route::post('/sys/dict',[SystemController::class,'createDict']);
-    Route::put('/sys/dict',[SystemController::class,'createDict']);
-    Route::put('/sys/dict/{id}',[SystemController::class,'deleteDict']);
-    Route::get('/sys/dictdata/page',[SystemController::class,'getDictPage']);
-    Route::post('/sys/dictdata',[SystemController::class,'createDictData']);
-    Route::put('/sys/dictdata',[SystemController::class,'createDictData']);
-    Route::put('/sys/dictdata/{id}',[SystemController::class,'deleteDictData']);
+    Route::get('/sys/dict',[DictController::class,'getDict']);
+    Route::post('/sys/dict',[DictController::class,'createDict']);
+    Route::put('/sys/dict',[DictController::class,'createDict']);
+    Route::put('/sys/dict/{id}',[DictController::class,'deleteDict']);
+    Route::get('/sys/dictdata/page',[DictController::class,'getDictPage']);
+    Route::post('/sys/dictdata',[DictController::class,'createDictData']);
+    Route::put('/sys/dictdata',[DictController::class,'createDictData']);
+    Route::put('/sys/dictdata/{id}',[DictController::class,'deleteDictData']);
 });
